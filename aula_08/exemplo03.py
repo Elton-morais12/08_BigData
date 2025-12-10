e0eb14991286c6222a4a776e6a938abc9ce43ad4
Elton-morais12 <eltonsouzademorais@gmail.com>
2025-12-09 21:55:03 -0300

    def saudacao

---
+++ aula_08/exemplo03.py
@@ -0,0 +1,29 @@
+def saudacao(n, i):
+    print(f'Olá, {n},o total é {i}')
+
+
+def soma(n1,n2):
+    total = n1 + n2
+    print(f'O total é: {total}')
+
+
+num1 = float(input('Digite um número: '))
+num2 = float(input('Digite outro número: '))
+soma(num1, num2)
+
+
+
+
+
+
+
+
+
+
+
+# nome = input('Informe o nome: ')
+# idade = input('Informe a idade: ')
+# saudacao(nome, idade)    
+    
+
+
